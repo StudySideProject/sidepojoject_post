e28ff97c74292064a3a51b2c40738eefed64e847
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2023-03-20 23:51:28 +0900

    Add : repository

---
+++ src/main/java/com/example/study_sideproject/StudySideprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_sideproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySideprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySideprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/study_sideproject/StudySideprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_sideproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySideprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
