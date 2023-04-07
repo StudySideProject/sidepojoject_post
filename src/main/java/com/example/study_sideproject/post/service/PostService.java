e3e39d4bd68b1936468e1fc0d9ab75528a786095
soojin-dev <dvxz1125@gmail.com>
2023-04-07 22:59:56 +0900

    FEAT : 필요없는 Import 삭제

--- src/main/java/com/example/study_sideproject/post/service/PostService.java
+++ src/main/java/com/example/study_sideproject/post/service/PostService.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor // final이나 @NonNull인 필드 값만 파라미터로 받는 생성자 생성
