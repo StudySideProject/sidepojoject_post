9579899cb738736e9c7ff4422c73e4b6206bf70e
soojin-dev <dvxz1125@gmail.com>
2023-04-09 16:11:14 +0900

    FEAT : 댓글 생성 기능 추가, 로그인여부 확인 등 검증메서드 validateCheck 클래스로 이동

---
+++ src/main/java/com/example/study_sideproject/comment/dto/CommentReqDto.java
@@ -0,0 +1,15 @@
+package com.example.study_sideproject.comment.dto;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentReqDto {
+
+    @Size(min = 3, max = 100) // 유효성 검사를 위해 사용
+    private String content;
+}
--- src/main/java/com/example/study_sideproject/post/service/PostService.java
+++ src/main/java/com/example/study_sideproject/post/service/PostService.java
@@ -1,23 +1,18 @@
 package com.example.study_sideproject.post.service;
 
-import com.example.study_sideproject.global.exception.CustomException;
-import com.example.study_sideproject.global.exception.ErrorCode;
-import com.example.study_sideproject.global.jwt.SecurityUtil;
+import com.example.study_sideproject.global.ValidateCheck;
 import com.example.study_sideproject.member.domain.Member;
 import com.example.study_sideproject.member.repository.MemberRepository;
 import com.example.study_sideproject.post.domain.Post;
 import com.example.study_sideproject.post.dto.request.PostReqDto;
 import com.example.study_sideproject.post.dto.response.AllPostResDto;
 import com.example.study_sideproject.post.dto.response.PostResponseDto;
-import com.example.study_sideproject.post.exception.customException.MemberInfoNotExistException;
-import com.example.study_sideproject.post.exception.customException.PostInfoNotExistException;
 import com.example.study_sideproject.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor // final이나 @NonNull인 필드 값만 파라미터로 받는 생성자 생성
@@ -25,12 +20,13 @@ public class PostService {
 
 	private final PostRepository postRepository;
 	private final MemberRepository memberRepository;
+	private final ValidateCheck validateCheck;
 
 	// 게시글 생성
 	@Transactional
 	public void createPost(PostReqDto postReqDto) {
 
-		Member member = getMemberIfExists();
+		Member member = validateCheck.getMemberIfExists();
 
 		Post post = Post.builder()
 				.title(postReqDto.getTitle())
@@ -53,7 +49,7 @@ public class PostService {
 	@Transactional(readOnly = true)
 	public PostResponseDto getOnePost(Long id){
 
-		Post post = getPostIfExists(id);
+		Post post = validateCheck.getPostIfExists(id);
 
 		return PostResponseDto.builder()
 				.id(post.getId())
@@ -68,7 +64,7 @@ public class PostService {
 	// 게시글 수정
 	@Transactional
 	public void updatePost(Long id, PostReqDto postReqDto) {
-		Post post = validateAuthor(id);
+		Post post = validateCheck.validateAuthor(id);
 		post.updatePost(postReqDto);
 	}
 
@@ -76,29 +72,7 @@ public class PostService {
 	// 게시글 삭제
 	@Transactional
 	public void deletePost(Long id) {
-		Post post = validateAuthor(id);
+		Post post = validateCheck.validateAuthor(id);
 		postRepository.deleteById(post.getId());
-	}
-
-	// 유효한 멤버인지 검증
-	private Member getMemberIfExists() {
-		// 현재 로그인한 사람이 있을 경우 해당 회원의 email을 가져온다.
-		String email = SecurityUtil.getCurrentUserEmail().orElseThrow(MemberInfoNotExistException::new);
-		// mysql에서 해당 email을 가진 회원이 없을 경우 예외발생, 있으면 회원 정보를 가져온다.
-		return memberRepository.findByEmail(email).orElseThrow(MemberInfoNotExistException::new);
-	}
-
-	// 게시글이 존재하는지 확인
-	private Post getPostIfExists(Long id) {
-		// mysql에서 해당 id에 해당하는 post가 없을 경우 예외발생, 있으면 post 정보를 가져온다.
-		return postRepository.findById(id).orElseThrow(PostInfoNotExistException::new);
-	}
-
-	// 게시글 작성자인지 확인
-	private Post validateAuthor(Long id) {
-		getPostIfExists(id);
-		Long memberId = getMemberIfExists().getId();
-		return postRepository.findByIdAndMemberId(id, memberId).orElseThrow(
-				() -> new CustomException(ErrorCode.NOT_AUTHOR));
 	}
 }
